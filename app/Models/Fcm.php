fc2cabe17994d0d0864f147fdf186550f623203f
NAD <lualua0909@gmail.com>
2019-07-29 13:15:30 +0700

    add fcm firebase

--- app/Models/Fcm.php
+++ app/Models/Fcm.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use LaravelFCM\Facades\FCM;
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
 
-class Fcm extends Model
+class FcmDevice extends Model
 {
     protected $table = "devices";
 
@@ -20,6 +24,47 @@ class Fcm extends Model
             return DB::table('devices')->insertGetId(
                 ['user_id' => $user_id, 'fcm' => $fcm]
             );
+        }
+
+    }
+
+    public static function sendMsgToDevice($token, $title, $body, $data = [])
+    {
+        try {
+            $optionBuilder = new OptionsBuilder();
+            $optionBuilder->setTimeToLive(60 * 20);
+
+            $notificationBuilder = new PayloadNotificationBuilder($title);
+            $notificationBuilder->setBody($body)
+                ->setSound('default');
+
+            $dataBuilder = new PayloadDataBuilder();
+            $dataBuilder->addData(['payload' => $data]);
+
+            $option = $optionBuilder->build();
+            $notification = $notificationBuilder->build();
+            $data = $dataBuilder->build();
+            $downstreamResponse = FCM::sendTo($token, $option, $notification, $data);
+
+            $downstreamResponse->numberSuccess();
+            $downstreamResponse->numberFailure();
+            $downstreamResponse->numberModification();
+
+            //return Array - you must remove all this tokens in your database
+            $downstreamResponse->tokensToDelete();
+
+            //return Array (key : oldToken, value : new token - you must change the token in your database )
+            $downstreamResponse->tokensToModify();
+
+            //return Array - you should try to resend the message to the tokens in the array
+            $downstreamResponse->tokensToRetry();
+            return true;
+
+            // return Array (key:token, value:errror) - in production you should remove from your database the tokens
+        } catch (\Exception $exception) {
+            dd($exception->getMessage());
+            logger(['service' => 'FCM Notification', 'content' => $exception->getMessage()]);
+            return false;
         }
 
     }
